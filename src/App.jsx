f882421c1d3a6b87289fe445fba38009f5e9b02d
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-20 02:04:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bg-blue-900 text-white min-h-screen">
       {/* HERO SECTION */}
-      <header className="relative h-screen flex items-center justify-center text-center bg-cover bg-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1603052877633-016e2291f7c3?auto=format&fit=crop&w=1500&q=80')" }}>
+      <header className="relative h-screen flex items-center justify-center text-center bg-cover bg-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1517927033932-b3d18e61fb3a?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
         <div className="absolute inset-0 bg-black bg-opacity-60"></div>
         <div className="relative z-10 px-6">
           <h1 className="text-5xl md:text-6xl font-bold text-yellow-400 mb-4 drop-shadow-lg">Welcome to Obazz FC</h1>
