f7032cb76fe427b65fb43fea055d28db6eef91ed
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-20 15:08:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ function App() {
 
   const slides = [
     {
-      bg: 'https://images.unsplash.com/photo-1517927033932-b3d18e61fb3a?q=80&w=1938',
+      bg:'/slide1.jpg',
       title: 'Welcome to Obazz FC',
       subtitle: 'Pride. Passion. Performance.',
       link: '#matches',
       button: 'View Fixtures'
     },
     {
-      bg: 'https://images.unsplash.com/photo-1430232324554-8f4aebd06683?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      bg: '/slide2.jpg',
       title: 'Feel the Thrill of the Game',
       subtitle: 'Join our journey to glory',
       link: '#news',
