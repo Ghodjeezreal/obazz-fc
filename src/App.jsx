10ee6229e75a1c85644507e3dd39dcbfad792c44
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-20 01:46:51 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="text-center text-yellow-400 bg-blue-900 min-h-screen">
+      <h1 className="text-4xl font-bold p-8">Welcome to Obazz FC</h1>
+      <p>Official Football Club Website</p>
+    </div>
+  );
+}
+
+export default App;
